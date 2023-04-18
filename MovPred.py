9b5988cfb9166ba2e9786df9af068a17e90af27c
Mohamed Ashraf <arzoqny@gmail.com>
2023-04-18 02:30:41 +0200

    first try

--- MovPred.py
+++ MovPred.py
@@ -3,11 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn import metrics 
+from scipy.stats import zscore
+
 movies =pd.read_csv("movies-regression-dataset.csv")
-
-
-
-
 
 #this block is for cleaning missing data 
 ######################################################
@@ -38,15 +36,8 @@ else:
     # Impute missing values with mean
     movies = movies['budget'].fillna(movies.mean())
 
-
-
 #drop dupes 
 movies = movies.drop_duplicates()
-
-
-
-
-
 
 # Fix inconsistent values in categorical columns by replacing with a standardized value
 movies['status'].replace(['Rumored', 'Post Production'], 'Unknown', inplace=True)
@@ -57,8 +48,26 @@ movies.loc[movies['vote_count'] < 10, 'vote_average'] = 0
 
 # Fix inconsistent date format in release_date column
 movies['release_date'] = pd.to_datetime(movies['release_date'], format='%m/%d/%Y')
+num_cols = ['budget', 'viewercount', 'revenue', 'runtime', 'vote_count', 'vote_average']
+removed = 0 
+movies[num_cols] = movies[num_cols].apply(zscore)
+for col in num_cols:
+    z_scores = zscore(movies[col])
+    abs_z_scores = np.abs(z_scores)
+    filtered_entries = (abs_z_scores < 4)
+    print((abs_z_scores < 4))
+    movies = movies[filtered_entries]
+# create boxplot for each numerical column
+plt.figure(figsize=(10, 6))
+movies[num_cols].boxplot()
+plt.xticks(rotation=45)
+plt.show()
+# print(movies[1])
+
+
+# Remove outliers using Z-score method
+cols = ['budget', 'revenue', 'vote_average', 'vote_count']
+
+# print(movies.columns)
 
 ######################################################
-
-
-
