49f633b6c8267266bbec31a45d96aedc31214825
Youssef Shehata <31982488+Youssef-Shehata@users.noreply.github.com>
2023-05-23 07:22:47 +0300

    last update

--- Milstone 2/Classification.py
+++ Milstone 2/Classification.py
@@ -8,6 +8,9 @@ from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np
+from sklearn.feature_selection import SelectKBest, mutual_info_regression
+
+import time
 import pickle
 from sklearn.model_selection import GridSearchCV
 
@@ -24,10 +27,7 @@ import matplotlib.pyplot as plt
 def main():
     # Read the movies from the CSV file
     movies = pd.read_csv('movies-classification-dataset.csv' , parse_dates=['release_date'])
-    # iterating the columns
-    # for col in movies.columns:
-    #     print(col)
-    # print(movies['keywords'].head(5))
+
     preprocess_titles(movies, 'original_title')
     preprocess_titles(movies, 'title')
 
@@ -41,9 +41,17 @@ def main():
 
     }
     movies['Rate']=movies['Rate'].map(ratemappins)
-    movies = clean_and_tokenize_tagline(movies,"tagline")
+    # movies = clean_and_tokenize_tagline(movies,"tagline")
     movies = clean_and_encode_homepage(movies,'homepage')
+    clean_and_tokenize(movies , 'overview')
+    clean_and_tokenize(movies , 'tagline')
+    movies = CatEncoding(movies,'genres')
+    movies = CatEncoding(movies,'keywords')
+    # movies['overview'] = ListEncoding(movies['overview'], 14)
+    # movies['tagline'] = ListEncoding(movies['tagline'], 4)
 
+
+    movies =LabelEncoding(movies)
     movies['homepage_freq_enc'].fillna(0, inplace=True)
     # movies = Keyencoding(movies)
     movies['overview'].fillna('', inplace=True)
@@ -55,11 +63,9 @@ def main():
 
     movies['release_date'] =  pd.to_datetime(movies.index)
 
-    # Convert the datetime object to a Unix timestamp
-    # movies['release_date'] = movies['release_date'].to_timestamp()
 
     # Cast the Unix timestamp to a float
-    movies['release_date'] = movies['release_date'].astype('int')
+    movies['release_date'] = movies['release_date'].astype('int64').astype('int32')
 
     movies = handleMissingNumValues(movies, 'budget')
     movies = handleMissingNumValues(movies, 'viewercount')
@@ -68,18 +74,11 @@ def main():
     movies = handleMissingNumValues(movies, 'vote_count')
 
     # drop dupes
-    movies.drop(['id', 'original_title', 'title' ,'tagline', 'keywords'], axis=1, inplace=True)
+    movies.drop(['id', 'original_title', 'title' ,'tagline', "overview"], axis=1, inplace=True)
      
 
     movies = movies.drop_duplicates()
-    movies = preOverview(movies , 'overview')
 
-    total = movies.shape[0]
-    threshold = total * .0005
-    cols = ['overview'] 
-    movies = movies.apply(lambda x:x.mask(x.map(x.value_counts()) < threshold, 'RARE') if x.name in cols else x)
-    movies = pd.get_dummies(data = movies , columns = cols)
-    movies.drop(['overview_RARE'  ], axis=1, inplace=True)
 
 
     # Extract the columns that need to be scaled
@@ -92,7 +91,6 @@ def main():
     # Apply the scaler to the selected columns
     movies[cols_to_scale] = scaler.fit_transform(movies[cols_to_scale])
 
-    movies = CatEncoding(movies)
 
     movies = cleanOutliers(movies)
 
@@ -113,86 +111,179 @@ def main():
     scaler = StandardScaler()
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
+    # Apply feature selection
+    def my_score(X, y):
+        return mutual_info_regression(X, y, random_state=0)
+
+    # selector = SelectKBest(score_func=my_score, k=5)
+    
+    # X_train = selector.fit_transform(X_train, y_train)
+    # X_test = selector.transform(X_test)
+    # Save the dictionary to a file
+    import json 
+    with open("classificationFeatures.json", "w") as f:
+        json.dump(X_test.tolist(), f, indent=4)
+
 
     # Train the models
     svm_model = SVC()
+    start_time = time.time()
     svm_model.fit(X_train, y_train)
+    training_time_svm = time.time() - start_time
 
     decision_tree_model = DecisionTreeClassifier()
+    start_time = time.time()
     decision_tree_model.fit(X_train, y_train)
+    training_time_decision = time.time() - start_time
 
     random_forest_model = RandomForestClassifier()
+    start_time = time.time()
     random_forest_model.fit(X_train, y_train)
+    training_time_random_forest = time.time() - start_time
 
     # Evaluate the models
+    start_time = time.time()
     svm_accuracy = svm_model.score(X_test, y_test)
+    testing_time_svm = time.time() - start_time
+
+    start_time = time.time()
     decision_tree_accuracy = decision_tree_model.score(X_test, y_test)
+    testing_time_decision = time.time() - start_time
+
+    start_time = time.time()
     random_forest_accuracy = random_forest_model.score(X_test, y_test)
+    testing_time_random_forest = time.time() - start_time
 
     print('SVM accuracy:', svm_accuracy)
     print('Decision tree accuracy:', decision_tree_accuracy)
     print('Random forest accuracy:', random_forest_accuracy)
 
-    max_features_range = np.arange(1,6,1)
-    n_estimators_range = np.arange(10,11,10)
+    accuracy_values_plot = [svm_accuracy, decision_tree_accuracy,
+                            random_forest_accuracy]  # Example accuracy values
+    model_labels = ['SVM', 'Decision_Tree', 'Random_forest']  # Example model labels
+
+    plt.bar(model_labels, accuracy_values_plot, width=0.7)
+
+    plt.xlabel('Models')
+    plt.ylabel('Accuracy')
+    plt.title('Model Accuracy Comparison before hyperparameter tunning')
+    plt.yticks([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+               ['10%', '20%', '30%', '40%', '50%', '60%', '70%', '80%', '90%', '100%'])
+    # plt.xticks(rotation=45)
+    plt.ylim(0, 1)  # Set the y-axis limits (0 to 1 in this case)
+
+    plt.show()
+
+    training_time_plot = [training_time_svm, training_time_decision,
+                          training_time_random_forest]  # Example accuracy values
+
+    plt.bar(model_labels, training_time_plot, width=0.7)
+
+    plt.xlabel('Models')
+    plt.ylabel('Time by seconds')
+    plt.title('Model Training Time Comparison before hyperparameter tunning')
+    # plt.yticks([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+    #            ['10%', '20%', '30%', '40%', '50%', '60%', '70%', '80%', '90%', '100%'])
+    # plt.xticks(rotation=45)
+    # plt.ylim(0, 1)  # Set the y-axis limits (0 to 1 in this case)
+
+    plt.show()
+
+
+    max_features_range = np.arange(1, 6, 1)
+    n_estimators_range = np.arange(10, 11, 10)
     rf_param_grid = dict(max_features=max_features_range, n_estimators=n_estimators_range)
     svm_param_grid = {'kernel': ['linear', 'poly', 'rbf'], 'C': [1, 10, 100]}
     decTree_param_grid = {'criterion': ['gini', 'entropy'], 'max_depth': [3, 5, 7]}
 
-    # hyperparameter tunningfor random forest 
+    # hyperparameter tunningfor random forest
     grid = GridSearchCV(estimator=random_forest_model, param_grid=rf_param_grid, cv=5)
+    start_time = time.time()
     grid.fit(X_train, y_train)
+    training_time_random_after = time.time() - start_time
+
     print("The best random forest parameters are %s with a score of %0.2f"
-      % (grid.best_params_, grid.best_score_))
-    
-    # hyperparameter tunningfor svm
+          % (grid.best_params_, grid.best_score_))
+    random_forest_accuracy_plot = grid.best_score_
+
+    # hyperparameter tunning for svm
     grid = GridSearchCV(svm_model, param_grid=svm_param_grid, cv=5)
+    start_time = time.time()
     grid.fit(X_train, y_train)
+    training_time_svm_after = time.time() - start_time
     print("The best svm parameters are %s with a score of %0.2f"
-      % (grid.best_params_, grid.best_score_))
-    
-    # hyperparameter tunningfor random forest 
+          % (grid.best_params_, grid.best_score_))
+    svm_accuracy_plot = grid.best_score_
+
+    # hyperparameter tunning for decision tree
     grid = GridSearchCV(estimator=decision_tree_model, param_grid=decTree_param_grid, cv=5)
+    start_time = time.time()
     grid.fit(X_train, y_train)
+    training_time_decision_after = time.time() - start_time
+
     print("The best decision tree forest parameters are %s with a score of %0.2f"
-      % (grid.best_params_, grid.best_score_))
-    
+          % (grid.best_params_, grid.best_score_))
+    decision_tree_accuracy_plot = grid.best_score_
+
+
+    # Select the best model
+    best_model = max(svm_model, decision_tree_model, random_forest_model, key=lambda model: model.score(X_test, y_test))
+
+    accuracy_values_plot = [svm_accuracy_plot, decision_tree_accuracy_plot, random_forest_accuracy_plot]  # Example accuracy values
+    model_labels = ['SVM', 'Decision_Tree', 'Random_forest']  # Example model labels
+
+    plt.bar(model_labels, accuracy_values_plot,width=0.7)
+
+    plt.xlabel('Models')
+    plt.ylabel('Accuracy')
+    plt.title('Model Accuracy Comparison after hyperparameter tunning')
+    plt.yticks([0.1, 0.2, 0.3, 0.4, 0.5,0.6,0.7,0.8,0.9,1.0], ['10%', '20%', '30%', '40%', '50%', '60%', '70%', '80%', '90%', '100%'])
+    # plt.xticks(rotation=45)
+    plt.ylim(0, 1)  # Set the y-axis limits (0 to 1 in this case)
+
+    plt.show()
+
+    training_time_plot_after = [training_time_svm_after, training_time_decision_after, training_time_random_after]  # Example accuracy values
+
+    plt.bar(model_labels, training_time_plot_after, width=0.7)
+
+    plt.xlabel('Models')
+    plt.ylabel('Time by seconds')
+    plt.title('Model Training Time Comparison after hyperparameter tunning')
+    # plt.yticks([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+    #            ['10%', '20%', '30%', '40%', '50%', '60%', '70%', '80%', '90%', '100%'])
+    # plt.xticks(rotation=45)
+    # plt.ylim(0, 1)  # Set the y-axis limits (0 to 1 in this case)
+
+    plt.show()
+
+    testing_time_plot = [testing_time_svm, testing_time_decision,testing_time_random_forest]  # Example accuracy values
+    plt.bar(model_labels, testing_time_plot, width=0.7)
+
+    plt.xlabel('Models')
+    plt.ylabel('Time by seconds')
+    plt.title('Model Testing Time Comparison')
+    # plt.yticks([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+    #            ['10%', '20%', '30%', '40%', '50%', '60%', '70%', '80%', '90%', '100%'])
+    # plt.xticks(rotation=45)
+    # plt.ylim(0, 1)  # Set the y-axis limits (0 to 1 in this case)
+
+    plt.show()
+
+    import json
+
+
+    # Save the models
+    pickle.dump(svm_model, open("svm_model.pkl", "wb"))
+    pickle.dump(random_forest_model, open("random_forest_model.pkl", "wb"))
+    pickle.dump(decision_tree_model, open("decision_tree_model.pkl", "wb"))
+
 
     # Select the best model
     best_model = max(svm_model, decision_tree_model, random_forest_model, key=lambda model: model.score(X_test, y_test))
 
 
    
-
-def preOverview(movies,coulmn_name):
-    # Initialize the lemmatizer and stop words
-    lemmatizer = WordNetLemmatizer()
-    stop_words = set(stopwords.words('english'))
-
-    # Define the pattern to remove non-ASCII characters
-    pattern = r'[^\x00-\x7F]+'
-
-    # Iterate over the rows in the specified column
-    for i, row in movies.iterrows():
-        content = row[coulmn_name]
-
-        # Remove non-ASCII characters
-        content_clean = re.sub(pattern, '', content)
-
-        # Tokenize the content into sentences and words
-        tokens = nltk.word_tokenize(content_clean)
-
-        # Remove stop words and punctuation
-        tokens_filtered = [lemmatizer.lemmatize(word) for word in tokens if
-                           word.casefold() not in stop_words and word.casefold() not in string.punctuation]
-
-        # Join the filtered tokens back into a string
-        clean_text = ' '.join(tokens_filtered)
-
-        # Update the value in the specified column with the cleaned text
-        movies.at[i, coulmn_name] = clean_text
-        return movies
-
 
 def preprocess_titles(df, column_name):
     # remove leading and trailing whitespaces
@@ -270,72 +361,48 @@ def cleanOutliers(movies):
         movies = movies.drop(outliers)
     return movies
 
-
-def CatEncoding(df):
-    def update_genres(x):
-
+def CatEncoding(df, column_name):
+    def update_column(x):
         import json
-
         json_string = x
-
-        # parse the JSON string into a Python object
-        movies = json.loads(json_string)
-
-        # iterate through the list of dictionaries and extract the values of the "name" key
-        names = [d["name"] for d in movies]
+        data = json.loads(json_string)
+        names = [d["name"] for d in data]
         return names
-        # apply function to genres column
 
-    df['genres'] = df['genres'].apply(lambda x: update_genres(x))
-    unique_genres = set(genre for movie_genres in df['genres'] for genre in movie_genres)
-    # print(unique_genres)
-    # Output: {'Drama', 'War', 'Action', 'Documentary', 'Comedy', 'Horror', 'Music', 'Crime', 'Thriller', 'Romance'}
-    # Create a new moviesframe with one column for each unique genre
-    for genre in unique_genres:
-        df[genre] = df['genres'].apply(lambda x: 1 if genre in x else 0)
+    df[column_name] = df[column_name].apply(lambda x: update_column(x))
 
-    # Drop the original 'genres' column
-    df.drop('genres', axis=1, inplace=True)
+    column_counts = df[column_name].explode().value_counts()
+    threshold = int(5/ 100 * len(df))  # Calculate the threshold based on the dataset size
+    popular_columns = column_counts[column_counts > threshold].index.tolist()
 
+    for column in popular_columns:
+        df[column] = df[column_name].apply(lambda x: 1 if column in x else 0)
+
+    df.drop(column_name, axis=1, inplace=True)
+
+
+
+    return df
+
+
+from sklearn.preprocessing import LabelEncoder
+
+
+def LabelEncoding(df):
     features = ['original_language', 'production_countries', 'spoken_languages', 'production_companies']
-
-    # Loop over each feature and label encode it
     for feature in features:
         le = LabelEncoder()
         df[feature] = le.fit_transform(df[feature].astype(str))
-    # Define the two possible values of the 'status' column
-    status_values = ['Released', 'Post Production', 'Unknown']
 
-    # Fit the encoder to the status values
+    status_values = ['Released', 'Post Production', 'Unknown']
     le.fit(status_values)
     df['status'] = df['status'].apply(lambda x: 'Unknown' if x not in le.classes_ else x)
-
-    # Apply label encoding to the 'status' column
     df['status'] = le.transform(df['status'])
 
     return df
 
-    # # Extract the genres column as a list
-    # genres_list = movies['genres'].str.split('|')
-
-    # # Get a list of all unique genres
-    # unique_genres = set([genre for genres in genres_list for genre in genres])
-
-    # # Create a dictionary mapping each unique genre to a binary column
-    # genre_dict = {}
-    # for genre in unique_genres:
-    #     genre_dict[genre] = [1 if genre in genres else 0 for genres in genres_list]
-    # print(genre_dict)
-    # # Create a moviesframe from the dictionary and concatenate it to the original moviesset
-    # genre_movies = pd.moviesFrame(genre_dict)
-    # movies = pd.concat([movies, genre_movies], axis=1)
-
-    # # Drop the original 'Genres' column
-    # movies.drop('genres', axis=1, inplace=True)
 
 
-
-import category_encoders as ce
 
 def clean_and_encode_homepage(df, column_name):
     # Extract domain name from URL
@@ -358,14 +425,47 @@ import nltk
 from nltk.corpus import stopwords
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-
-def clean_and_tokenize_tagline(df, column_name):
-    # Remove punctuation, convert to lowercase, and tokenize text while removing stop words
+def clean_and_tokenize(df, column_name):
+    lemmatizer = WordNetLemmatizer()
     stop_words = set(stopwords.words('english'))
-    df[column_name] = df[column_name].apply(lambda x: tuple(word for word in nltk.word_tokenize(str(x).lower().replace('.', '').replace(',', '').replace('?', '')) if word not in stop_words))
+
+    def remove_non_ascii(text):
+        return "".join([char for char in text if ord(char) < 128 or char == "'"])
+
+    df[column_name].fillna("", inplace=True)
+
+    df[column_name] = df[column_name].apply(lambda x: [
+        lemmatizer.lemmatize(word) for word in nltk.word_tokenize(remove_non_ascii(str(x).lower()))
+        if word not in stop_words and all(char not in string.punctuation for char in word) and word != ""
+    ])
 
     return df
 
+
+
+def ListEncoding(column, threshold_percentage):
+    def update_column(x):
+        names = x
+        return names
+
+    column = column.apply(lambda x: update_column(x))
+
+    # Filter out values that are not lists or have different lengths
+    column = column.apply(lambda x: x if isinstance(x, list) and len(x) == len(column) else [])
+
+    column_counts = column.explode().value_counts()
+    threshold = int(threshold_percentage / 100 * len(column))  # Calculate the threshold based on the dataset size
+    popular_columns = column_counts[column_counts > threshold].index.tolist()
+
+    print("Popular Columns:", popular_columns)
+
+    df = pd.DataFrame()
+    for col in popular_columns:
+        df[col] = column.apply(lambda x: 1 if col in x else 0)
+
+    print("Encoded DataFrame:", df)
+
+    return df
 def Keyencoding(df):
     def updateWords(x):
 
